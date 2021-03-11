581a450743a4afe170345be0605bb90145b7ac82
qiyupeng <1563990351@qq.com>
2021-03-11 17:02:09 +0800

    新建用户密码管理类

---
+++ src/main/java/com/q/auth/MqttListenerController.java
@@ -0,0 +1,15 @@
+package com.q.auth;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @Author: qyp
+ * @Date: 2021/3/11 17:00
+ * @Description:
+ */
+@RestController
+@RequestMapping("/user")
+public class MqttListenerController {
+}
